627bfc98aa623e171ff2bf909dcf05cddd98775c
Ahmad <ahmad.babaei@gmail.com>
2024-01-26 13:42:04 +0330

    feat: add teset project and some unit tests.

---
+++ TechTest.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TechTest.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TechTest.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79a31fb9-122f-4785-83e3-11b7367711f9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TechTest.Test/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using QuickGraph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using TechTest.ConsoleApp;
+using TechTest.ConsoleApp.Exceptions;
+
+namespace TechTest.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestDirectRoute()
+        {
+            CustomCurrencyConverter.Instance.ClearConfiguration();
+
+            CustomCurrencyConverter.Instance.UpdateConfiguration(InitData());
+
+            var result = CustomCurrencyConverter.Instance.Convert("USD", "CAD", 2000);
+
+            Assert.IsTrue(Math.Round(result, 2) == 2680.00);
+        }
+
+        [TestMethod]
+        public void TestIndirectRoute()
+        {
+            CustomCurrencyConverter.Instance.ClearConfiguration();
+
+            CustomCurrencyConverter.Instance.UpdateConfiguration(InitData());
+
+            var result = CustomCurrencyConverter.Instance.Convert("CAD", "EUR", 2000);
+
+            Assert.IsTrue(Math.Round(result, 2) == 1283.58);
+        }
+
+        [TestMethod]
+        public void TestNoConfigurationData()
+        {
+            CustomCurrencyConverter.Instance.ClearConfiguration();
+            //CustomCurrencyConverter.Instance.UpdateConfiguration(InitData());
+
+            Assert.ThrowsException<NoConfigurationDataException>(() => CustomCurrencyConverter.Instance.Convert("USD", "CAD", 2000));
+
+        }
+
+        private IEnumerable<Tuple<string, string, double>> InitData()
+        {
+            var initData = "" +
+                "(USD => CAD) 1.34\r\n" +
+                "(CAD => GBP) 0.58\r\n" +
+                "(USD => EUR) 0.86\r\n";
+
+            string[] convertedString = NormalizeInputData(initData);
+            return ConvertInputData(convertedString);
+        }
+
+        private IEnumerable<Tuple<string, string, double>> ConvertInputData(string[] convertedString)
+        {
+            // each line contains: 'CAD=>GBP:0.58'
+            return convertedString
+                .ToList()
+                .Select(line => line.Split(new[] { "=>", ":" }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(item =>
+                {
+                    var from = item[0];
+                    var to = item[1];
+                    var amount = double.Parse(item[2]);
+
+                    return new Tuple<string, string, double>(from, to, amount);
+                });
+        }
+
+        private string[] NormalizeInputData(string initData)
+        {
+            return initData
+                            .Replace("(", "")
+                            .Replace(")", ":")
+                            .Replace(" ", "")
+                            .Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+    }
+
+}
