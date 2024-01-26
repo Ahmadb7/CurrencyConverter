32e54caa1de71afb6d26e48c2b74bcde3704c523
Ahmad <ahmad.babaei@gmail.com>
2024-01-26 13:41:22 +0330

    fix: init data.

--- TechTest.ConsoleApp/Program.cs
+++ TechTest.ConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace TechTest.ConsoleApp
 {
@@ -7,12 +9,59 @@ namespace TechTest.ConsoleApp
 
         static void Main(string[] args)
         {
-            
+            //CustomCurrencyConverter.Instance.UpdateConfiguration(InitData());
+
             var from = "USD";
             var to = "EUR";
-            var result = CustomCurrencyConverter.Instance.Convert(from, to, 1000);
+            var result = -1d;
 
-            Console.WriteLine("");
+            try
+            {
+                result = CustomCurrencyConverter.Instance.Convert(from, to, 1000);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            Console.ReadLine();
+        }
+
+        private static IEnumerable<Tuple<string, string, double>> InitData()
+        {
+            var initData = "" +
+                "(USD => CAD) 1.34\r\n" +
+                "(CAD => GBP) 0.58\r\n" +
+                "(USD => EUR) 0.86\r\n";
+
+            string[] convertedString = NormalizeInputData(initData);
+            return ConvertInputData(convertedString);
+        }
+
+        private static IEnumerable<Tuple<string, string, double>> ConvertInputData(string[] convertedString)
+        {
+            // each line contains: 'CAD=>GBP:0.58'
+            return convertedString
+                .ToList()
+                .Select(line => line.Split(new[] { "=>", ":" }, StringSplitOptions.RemoveEmptyEntries))
+                .Select(item =>
+                {
+                    var from = item[0];
+                    var to = item[1];
+                    var amount = double.Parse(item[2]);
+
+                    return new Tuple<string, string, double>(from, to, amount);
+                });
+        }
+
+        private static string[] NormalizeInputData(string initData)
+        {
+            return initData
+                            .Replace("(", "")
+                            .Replace(")", ":")
+                            .Replace(" ", "")
+                            .Split(new[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
         }
 
     }
